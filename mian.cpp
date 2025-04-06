353c36361361f8833dc56e6acac0f1b4aee96f07
Bbqv <107970651+Bbqv@users.noreply.github.com>
2025-04-06 19:34:12 +0800

    beta6

--- mian.cpp
+++ mian.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(){
     cout << "hello world!" << endl;
     cout << "hello world!3" << endl;
+
+
+
     cout << "hello world3 beta" << endl;
     cout << "beta 5" << endl;
     return 0;
